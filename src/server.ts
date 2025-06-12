32d88fb098918d7d1070d4327a680ceaa7c13a7f
Juan Veronez <juan.monteirov@gmail.com>
2025-06-11 21:31:34 -0300

    build: start a server using fastify

---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.get('/', () => ({ message: 'Hello World!' }))
+
+app.listen({ port: 3333 }).then(() => console.log('🟢 Server started! 🟢'))
